42243dd3f1083009db76ca190640386e2cd938d8
Jono Shields <jonathonshields@gmail.com>
2020-12-29 08:59:33 +1300

    service worker

--- script/sw.js
+++ script/sw.js
@@ -1,12 +1,15 @@
 self.addEventListener('install', function(event) {
+    console.log("cache make");
     event.waitUntil(
       caches.open('sw-cache').then(function(cache) {
-        return cache.addAll(['/', '/index.html', '/script/app.js', '/style/style.css', '/icon.png']);
+        return cache.add('/');
       })
     );
   });
    
   self.addEventListener('fetch', function(event) {
+    console.log("cache fetch");
+    console.log(event.request);
     event.respondWith(
       caches.match(event.request).then(function(response) {
         return response || fetch(event.request);
