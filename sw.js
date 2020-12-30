73d8aa9b1501f4d118a8d5f461e4b516ed6a42b8
Jono Shields <jonathonshields@gmail.com>
2020-12-31 11:53:46 +1300

    fix for lighthouse

--- sw.js
+++ sw.js
@@ -1,6 +1,8 @@
+var CACHE = 'sw-cache';
+
 self.addEventListener('install', function(event) {
     event.waitUntil(
-      caches.open('sw-cache').then(function(cache) {
+      caches.open(CACHE).then(function(cache) {
         return cache.addAll([
             '/',
             '/index.html',
@@ -11,11 +13,25 @@ self.addEventListener('install', function(event) {
       })
     );
   });
-   
+
   self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
-      })
-    );
+    event.respondWith(fromCache(event.request));
+    event.waitUntil(update(event.request));
   });
+
+  function fromCache(request) {
+    return caches.open(CACHE).then(function (cache) {
+      return cache.match(request).then(function (matching) {
+        return matching || Promise.reject('no-match');
+      });
+    });
+  }
+
+  function update(request) {
+    return caches.open(CACHE).then(function (cache) {
+      return fetch(request).then(function (response) {
+        return cache.put(request, response);
+      });
+    });
+  }
+  
